7a3783a7e849dba4a05a982f8e0197ca7eb9fb41
saparatayev <sopa.97@mail.ru>
2020-10-26 15:43:04 +0500

    Add routing

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"workshop/internal/handler"
+
+	"github.com/go-chi/chi"
+)
+
 func main() {
 
+	h := handler.NewHandler()
+
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
