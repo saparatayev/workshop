200478b4c059deba430d95931638cf2e20819b3a
saparatayev <sopa.97@mail.ru>
2020-10-27 12:06:11 +0500

    Add config

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"workshop/internal/config"
 	"workshop/internal/handler"
 
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	h := handler.NewHandler()
 
@@ -16,9 +23,11 @@ func main() {
 
 	r.Get("/hello", h.Hello)
 
-	log.Print("starting server")
+	path := cfg.Host + ":" + cfg.Port
 
-	log.Fatal(http.ListenAndServe(":8081", r))
+	log.Printf("starting server at %s", path)
+
+	log.Fatal(http.ListenAndServe(path, r))
 
 	log.Print("shutting server down")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module workshop
 
 go 1.14
 
-require github.com/go-chi/chi v4.1.2+incompatible
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/ilyakaznacheev/cleanenv v1.2.5
+)
